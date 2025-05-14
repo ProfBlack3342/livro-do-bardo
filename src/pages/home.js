eab95abecb85fc52be28849804617c69ca234c3a
ProfBlack3342 <dudu.pm@hotmail.com>
2025-05-14 18:35:35 -0300

    Criação home.js

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+
+function PageHome() {
+    return (
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
+        </main>
+    );
+}
+
+export default PageHome;
